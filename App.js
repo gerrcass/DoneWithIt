914217ec30054e505e19310aa1d84c1be60d91e1
Gerardo Castillo <gerrcass@gmail.com>
2020-10-08 15:28:46 -0400

    Using react-native FlatList to render a list of items in a custom component...

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+//import Card from "./app/components/Card";
+//import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+//import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   //return <ListingDetailsScreen />;
-  return <ViewImageScreen />;
+  //return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
