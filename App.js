e98a976a0d393eedc65cb5864acfcbbdb7f7b610
Gerardo Castillo <gerrcass@gmail.com>
2020-10-10 05:46:44 -0400

    Creating a reusable Icon component intended to be used in ListItem

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+//import { Text } from "react-native";
 
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 //import Card from "./app/components/Card";
 //import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+//import MessagesScreen from "./app/screens/MessagesScreen";
 //import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   //return <ListingDetailsScreen />;
   //return <ViewImageScreen />;
-  return <MessagesScreen />;
+  //return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 }
