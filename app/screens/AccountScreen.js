ab200ae35f8e6e7b794d30777d13d57aae81961c
Gerardo Castillo <gerrcass@gmail.com>
2020-10-11 05:04:45 -0400

    Finishing the account screen which consist of 3 sections (react native SectionList component can be used as well but is a little overkill for this litle screen).

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
+import Icon from "../components/Icon";
 import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
 import Screen from "../components/Screen";
+import colors from "../config/colors";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
 export default function AccountScreen() {
   return (
-    <Screen>
+    <Screen style={styles.screen}>
       <View style={styles.container}>
         <ListItem
           title="Mosh Hamedani"
@@ -14,6 +34,28 @@ export default function AccountScreen() {
           image={require("../assets/mosh.jpg")}
         />
       </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItems) => menuItems.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
     </Screen>
   );
 }
@@ -21,5 +63,8 @@ export default function AccountScreen() {
 const styles = StyleSheet.create({
   container: {
     marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
   },
 });
