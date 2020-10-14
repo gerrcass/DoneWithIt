c931c6f0c2e8314f035f96afd3797b106e340941
Gerardo Castillo <gerrcass@gmail.com>
2020-10-14 05:09:47 -0400

    Coding all moving parts in AppPicker component...

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Button,
+  FlatList,
   Modal,
   StyleSheet,
   TouchableWithoutFeedback,
@@ -11,12 +12,14 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
 import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
 export default function AppPicker({
   icon,
+  items,
+  onSelectItem,
   placeholder,
-  onPress,
-  ...otherProps
+  selectedItem,
 }) {
   const [modalVisible, setModalVisible] = useState(false);
   return (
@@ -31,7 +34,9 @@ export default function AppPicker({
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>{placeholder}</AppText>
+          <AppText style={styles.text}>
+            {selectedItem ? selectedItem.label : placeholder}
+          </AppText>
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -42,6 +47,19 @@ export default function AppPicker({
       <Modal visible={modalVisible} animationType="slide">
         <Screen>
           <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => {
+                  setModalVisible(false);
+                  onSelectItem(item);
+                }}
+              />
+            )}
+          />
         </Screen>
       </Modal>
     </>
