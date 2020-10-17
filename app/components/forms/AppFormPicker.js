868376dd7bd72108b76ce4e2f54d8dc0203faa23
Gerardo Castillo <gerrcass@gmail.com>
2020-10-17 12:40:06 -0400

    Building the CategoryPickerItem component to level up the code and making it much more scalable, reusable and clean.

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,13 +4,22 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-export default function AppFormPicker({ items, name, placeholder, width }) {
+export default function AppFormPicker({
+  items,
+  name,
+  numberOfColumns,
+  PickerItemComponent,
+  placeholder,
+  width,
+}) {
   const { errors, setFieldValue, touched, values } = useFormikContext();
   return (
     <>
       <AppPicker
         items={items}
         onSelectItem={(item) => setFieldValue(name, item)}
+        numberOfColumns={numberOfColumns}
+        PickerItemComponent={PickerItemComponent}
         placeholder={placeholder}
         selectedItem={values[name]}
         width={width}
