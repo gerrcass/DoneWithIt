74bd00226b3720edb6fa8740dd77967abb5ff4e6
Gerardo Castillo <gerrcass@gmail.com>
2020-10-26 07:07:34 -0400

    Adding a AccountNavigator to navigate through differents options in that screen.

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AccountScreen from "../screens/AccountScreen";
+import AccountNavigator from "./AccountNavigator";
 import FeedNavigator from "./FeedNavigator";
 import ListingEditScreen from "../screens/ListingEditScreen";
 
@@ -11,6 +11,6 @@ export default AppNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen name="Feed" component={FeedNavigator}></Tab.Screen>
     <Tab.Screen name="ListingEdit" component={ListingEditScreen}></Tab.Screen>
-    <Tab.Screen name="Account" component={AccountScreen}></Tab.Screen>
+    <Tab.Screen name="Account" component={AccountNavigator}></Tab.Screen>
   </Tab.Navigator>
 );
